dd37e608eacb2ef2730725c9ed46a3a2ac83409f
chocho77 <chavmom@gmail.com>
2022-09-17 21:02:32 +0300

    Changes to be committed: 	modified:   fullstack-front/src/components/Schedule.js

--- fullstack-front/src/components/Schedule.js
+++ fullstack-front/src/components/Schedule.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Inject,ScheduleComponent,Day, Week, WorkWeek, Month, Agenda } from '@syncfusion/ej2-react-schedule';
+import { Inject,ScheduleComponent,Day, Week, WorkWeek, Month, Agenda, EventSettingsModel} from '@syncfusion/ej2-react-schedule';
 
 const Schedule = () => {
   return (
